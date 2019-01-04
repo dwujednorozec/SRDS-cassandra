0abf97098f59d1b950782b9d5bfb32b6bb6cb6d5
dwujednorozec <jankesmiecho@gmail.com>
2019-01-04 22:23:44 +0100

    fix reader

--- srds.books/src/main/java/Reader.java
+++ srds.books/src/main/java/Reader.java
@@ -4,7 +4,8 @@ public class Reader implements Runnable {
     public void run(){
         Random generator = new Random();
         try {
-            if(){// wyporzyczamy jeżeli są dostępne WSZYSTKIE
+            if() {// wyporzyczamy jeżeli są dostępne WSZYSTKIE
+
             } else { ///odrzucamy wyporzycenie
 
             }
