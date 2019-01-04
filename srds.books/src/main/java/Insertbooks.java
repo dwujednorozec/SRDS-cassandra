b1e4a66dfbb03f358640623cde17d40fdb9e98d6
dwujednorozec <jankesmiecho@gmail.com>
2019-01-04 23:03:54 +0100

    add books to booktable

---
+++ srds.books/src/main/java/Insertbooks.java
@@ -0,0 +1,38 @@
+import CassBackend.Backend;
+
+public class Insertbooks {
+    public void insertbookByTitle() {
+
+        String query1 = "INSERT INTO book_table (id_book, book_name, nr_of_free_books, total_books)"
+
+                + " VALUES(1,'Potop', 10, 10);" ;
+
+        String query2 = "INSERT INTO book_table (id_book, book_name, nr_of_free_books, total_books)"
+
+
+                + " VALUES(1,'Dziady', 100, 120);" ;
+
+        String query3 = "INSERT INTO book_table (id_book, book_name, nr_of_free_books, total_books)"
+
+                + " VALUES(1,'Hari Pota', 20, 80);" ;
+
+        String query4 = "INSERT INTO book_table (id_book, book_name, nr_of_free_books, total_books)"
+
+                + " VALUES(1,'Hari Pota 2', 10, 180);" ;
+
+
+        Backend backend = new Backend("config.properties");
+
+
+        session = backend.getSession();
+
+        session.execute(query1);
+
+        session.execute(query2);
+
+        session.execute(query3);
+
+        session.execute(query4);
+    }
+
+}
--- srds.books/src/main/java/Main.java
+++ srds.books/src/main/java/Main.java
@@ -1,6 +1,4 @@
 import CassBackend.Backend;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.Cluster;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,39 +9,6 @@ public class Main {
         Backend backend = new Backend("config.properties");
 
         Books books = new Books(backend);
-    }
-
-    public void insertbookByTitle() {
-
-        String query1 = "INSERT INTO book_table (id_book, book_name, nr_of_free_books, total_books)"
-
-                + " VALUES(1,'Potop', 10, 10);" ;
-
-        String query2 = "INSERT INTO book_table (id_book, book_name, nr_of_free_books, total_books)"
-
-
-                + " VALUES(1,'Dziady', 100, 120);" ;
-
-        String query3 = "INSERT INTO book_table (id_book, book_name, nr_of_free_books, total_books)"
-
-                + " VALUES(1,'Hari Pota', 20, 80);" ;
-
-        String query4 = "INSERT INTO book_table (id_book, book_name, nr_of_free_books, total_books)"
-
-                + " VALUES(1,'Hari Pota 2', 10, 180);" ;
-        //Creating Cluster object
-        Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
-
-        //Creating Session object
-        Session session = cluster.connect("tp");
-
-        session.execute(query1);
-
-        session.execute(query2);
-
-        session.execute(query3);
-
-        session.execute(query4);
 
 
     }
