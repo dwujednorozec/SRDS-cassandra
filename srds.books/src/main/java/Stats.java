7cf7fe7f69990bc6b873a7e166239551db27d45e
dwujednorozec <jankesmiecho@gmail.com>
2019-01-04 21:36:30 +0100

    ad metods to books and reqbooks

---
+++ srds.books/src/main/java/Stats.java
@@ -0,0 +1,23 @@
+public class Stats {
+    private int id;
+    private int nr_of_free_books;
+    private int total_books;
+    private String book_name;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNumberOfFreeBooks() {
+        return nr_of_free_books;
+    }
+
+    public int getTotalBooks() {
+        return total_books;
+    }
+
+    public String getBookName() {
+        return book_name;
+    }
+
+}
