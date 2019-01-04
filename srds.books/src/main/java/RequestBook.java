00c2c8ffc85a6d9a3d9e79e15da729e0174ed7ca
Zertock <45946467+zertock@users.noreply.github.com>
2019-01-04 21:29:15 +0100

    updated req book

--- srds.books/src/main/java/RequestBook.java
+++ srds.books/src/main/java/RequestBook.java
@@ -1,3 +1,9 @@
+import com.datastax.driver.core.Session;
+import static com.datastax.driver.core.schemabuilder.SchemaBuilder.createTable;
+
+
+
+import java.util.UUID;
 
 
 public class RequestBook extends CassandraTableModel {
@@ -5,7 +11,37 @@ public class RequestBook extends CassandraTableModel {
     private int requestId;
     private int id_book;
     private int id_user;
-    private int availableBooks;
+    private int requestedBooks;
     private long timestamp;
+
+    public RequestBook(Session session, int requestId, int id_book, int id_user, int requestedBooks) {
+        super(session);
+        this.requestId = requestId;
+        this.id_book = id_book;
+        this.id_user = id_user;
+        this.requestedBooks = requestedBooks;
+    }
+
+// przerobienie na koszyk requestow: dodanie fora i powielanie requestu + dodanie UUID pojedynczego requestu
+
+    public void saveRequest() {
+        createTable(TABLE_NAME);
+
+        StringBuilder sb = new StringBuilder("INSERT INTO ")
+                .append(TABLE_NAME).append("(id, id_book, id_user, timestamp)")
+                .append("VALUES (").append(UUID.randomUUID())
+                .append(", ").append(id_book)
+                .append(", ").append(id_user)
+                .append(", ").append(requestedBooks)
+                .append(", ").append(System.currentTimeMillis())
+                .append(");");
+
+        String query = sb.toString();
+        execute(query);
+    }
+
+    public void checkApproved(int id_book, int id_user){
+        //todo
+    }
 
 }
