982bf224ea211c7b465527108edef23df957675c
dwujednorozec <jankesmiecho@gmail.com>
2019-01-04 21:31:01 +0100

    ad metods to books and reqbooks

--- srds.books/src/main/java/Books.java
+++ srds.books/src/main/java/Books.java
@@ -2,4 +2,24 @@
 
 public class Books {
     private int id;
+    private int nr_of_free_books;
+    private int total_books;
+    private string book_name;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNumberOfFreeBooks() {
+        return nr_of_free_books;
+    }
+
+    public int getTotalBooks() {
+        return total_books;
+    }
+
+    public string getBookName() {
+        return book_name;
+    }
+
 }
--- srds.books/src/main/java/RequestBook.java
+++ srds.books/src/main/java/RequestBook.java
@@ -8,4 +8,24 @@ public class RequestBook extends CassandraTableModel {
     private int availableBooks;
     private long timestamp;
 
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public int getBookID() {
+        return id_book;
+    }
+
+    public int getUserid() {
+        return id_user;
+    }
+
+    public int getAvailableBooks() {
+        return availableBooks;
+    }
+
+    public int getTimestamp() {
+        return timestamp;
+    }
 }
